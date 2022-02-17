551285c311c69a06740bd836fbe3f1f0c86bbe05
Paul Munyao <paulmunyao094@gmail.com>
2022-02-17 10:27:08 +0300

    Assign a varibale to class yes

--- madlibs.py
+++ madlibs.py
@@ -9,14 +9,14 @@
 # A Program that uses if and else statements
 
 name = str(input("Hello!What's your name?  "))
-print("Hello", name + "!", "Let's see if it's time to leave this party.")
+print("Hello",  name + "!", "Let's see if it's time to leave this party.")
 
 is_awkward = str(input("Is it awkward? "))
 
-# yes = is_awkward
-if ("yes"):
+# yes = "yes"
+if is_awkward is "yes":
     print("Let's leave this party")
-# elif ("no"):
+# elif "no":
 #         print("Let's stay and par-tay")
 else:
     print("Let's par-tay")
